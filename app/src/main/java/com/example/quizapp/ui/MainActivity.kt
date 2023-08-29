862ee0e78d1730c9b01f23e1a8d403e6aa783fa0
ermolenkoA <kypalo2012@gmail.com>
2023-08-29 19:30:18 +0300

    Created main screen prototype

--- app/src/main/java/com/example/quizapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/quizapp/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.quizapp
+package com.example.quizapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.quizapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/com/example/quizapp/ui/MainScreenFragment.kt
+++ app/src/main/java/com/example/quizapp/ui/MainScreenFragment.kt
@@ -1,10 +1,11 @@
-package com.example.quizapp
+package com.example.quizapp.ui
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.quizapp.R
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -13,10 +14,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [GameFragment.newInstance] factory method to
+ * Use the [MainScreenFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class GameFragment : Fragment() {
+class MainScreenFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -34,7 +35,7 @@ class GameFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_game, container, false)
+        return inflater.inflate(R.layout.fragment_main_screen, container, false)
     }
 
     companion object {
@@ -44,12 +45,12 @@ class GameFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment GameFragment.
+         * @return A new instance of fragment MainScreenFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            GameFragment().apply {
+            MainScreenFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
