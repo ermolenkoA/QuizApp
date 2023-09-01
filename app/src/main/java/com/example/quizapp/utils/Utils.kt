ffce9149dbf1af37056ea4ff2249f1563427923d
ermolenkoA <kypalo2012@gmail.com>
2023-09-01 04:07:33 +0300

    Added images

---
+++ app/src/main/java/com/example/quizapp/utils/Utils.kt
@@ -0,0 +1,13 @@
+package com.example.quizapp.utils
+
+import com.example.quizapp.data.Topic
+
+object Utils {
+    val dataset = arrayListOf<Topic>(
+        Topic(
+            id = 0,
+            title = "Animals",
+
+        )
+    )
+}
